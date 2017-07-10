2b1e523981236ce3e9b7721d7f5833ef8b9efe1c
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-10 12:31:51 +0200

    answer and ask methods created - link_to 'ask' created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'coaching/answer'
+  # get 'coaching/ask'
+  get 'answer', to: 'coaching#answer'
+  root to: 'coaching#ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
